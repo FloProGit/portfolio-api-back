50402d3b2c6a147ba539f60d92d590469bf3f7bd
florian <sudreflorianpro@gmail.com>
2026-03-08 19:19:40 +0100

    fix(controller) class name was create with Api/

--- src/Controller/Api/ApiAuthController.php
+++ src/Controller/Api/ApiAuthController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class Api/ApiAuthController extends AbstractController
+final class ApiAuthController extends AbstractController
 {
     #[Route('/api/api/auth', name: 'app_api_api_auth')]
     public function index(): Response
