0b0d458a996845fdcb471b4567dc6a331e57e6e3
florian <sudreflorianpro@gmail.com>
2026-03-08 18:56:48 +0100

    feat(fixture): create admin fixture and load

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('sudreflorianpro@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->hasher->hashPassword($admin, 'flopro951753654852')
+        );
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
